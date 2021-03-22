86cef47701ffdb1181c4acc1ad176e9ce1a6af28
Mia <miamorticia@Mias-MacBook-Pro.local>
2021-03-22 17:23:42 -0600

    color convert logic

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  return <h2>color tuner</h2>
+}
+
+export default App
