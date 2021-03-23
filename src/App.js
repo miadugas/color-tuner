7fae4f731976eb840c942bf262d6f5cbcee35df5
Mia <miamorticia@Mias-MacBook-Pro.local>
2021-03-22 18:26:42 -0600

    handle error logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,41 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color tuner</h2>
+  const [color,setColor] = useState('');
+  const [error,setError] = useState(false);
+  const [list,setList] = useState([]);
+  
+  const handleSubmit =(e) => {
+    e.preventDefault()
+    try {
+
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+    }
+  }
+
+
+  return (
+    <>
+  <section className="container">
+    <h3>color tuner</h3>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={color} onChange={(e) => setColor(e.target.value)}
+      placeholder="#00000" 
+      className={`${error ? 'error' : null}`}
+      />
+      <button className="btn" type="submit">
+        submit
+      </button>
+    </form>
+  </section>
+  <section className="colors">
+    {list.map}
+  </section>
+  </>
+  );
 }
 
 export default App
